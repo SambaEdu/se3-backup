56cce592336ae1e96f6415be031101e6fbd3535f
plouf <plouf@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-04-26 19:10:37 +0000

    Ajout du TAG svn

--- trunk/se3-backup/usr/share/se3/scripts/udev_disk_rule.sh
+++ trunk/se3-backup/usr/share/se3/scripts/udev_disk_rule.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#
+# $Id$
+#
+
 echo "ACTION==\"add\", KERNEL==\"sd*\", RUN+=\"/usr/share/se3/sbin/mountdisk.sh /dev/disk/by-id/$1-part1\"" >/etc/udev/rules.d/10-se3.rules
 echo "ACTION==\"remove\", KERNEL==\"sd*\", RUN+=\"/usr/share/se3/sbin/umountdisk.sh /dev/disk/by-id/$1-part1\"" >>/etc/udev/rules.d/10-se3.rules
 /etc/init.d/udev reload >/dev/null
