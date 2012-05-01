594b2f790f244ded72303497e1b9cf5f7bdbf3d2
keyser <mollef@users.noreply.github.com>
2012-05-01 00:56:52 +0000

    Ajout test montage du media afin d'éviter la saturation de /var

--- usr/share/se3/sbin/testbackup.sh
+++ usr/share/se3/sbin/testbackup.sh
@@ -6,56 +6,75 @@
 
 if [ "$1" = "--help" -o "$1" = "-h" ]
 then
-	echo "Test si le disque de sauvegarde est mont&#233;"
+	echo "Test si le disque de sauvegarde est monte"
 	echo "Usage : aucune option"
 	exit
 fi
-if [ -e /var/www/se3/includes/config.inc.php ]
-then
-        dbhost=`cat /var/www/se3/includes/config.inc.php | grep "dbhost=" | cut -d = -f 2 |cut -d \" -f 2`
-        dbname=`cat /var/www/se3/includes/config.inc.php | grep "dbname=" | cut -d = -f 2 |cut -d \" -f 2`
-        dbuser=`cat /var/www/se3/includes/config.inc.php | grep "dbuser=" | cut -d = -f 2 |cut -d \" -f 2`
-        dbpass=`cat /var/www/se3/includes/config.inc.php | grep "dbpass=" | cut -d = -f 2 |cut -d \" -f 2`
-else
-	echo "impossible d'acceder aux params mysql"
-	exit 1
-fi
 
-BPCMEDIA=`mysql se3db -u $dbuser -p$dbpass -B -N -e "select value from params where name='bpcmedia'"`
+#init params bdd
+. /usr/share/se3/includes/config.inc.sh -b
 
-if [ "$BPCMEDIA" = "1" ]; then
-##### Teste si le disque USB est mont&#233; #####
+if [ "$bpcmedia" = "1" ]; then
+##### Teste si le disque USB est monte #####
 
 mount |grep "\/var\/lib\/backuppc" >/dev/null || MBPC=0
 if [ "$MBPC" = "0" ]; then
-	echo "Disque USB non mont&#233;"
+	echo "Disque USB non monte"
 	EXIT1=1
 fi
 touch /var/lib/backuppc/test 2&> /dev/null || TBPC=0
 if [ "$TBPC" = "0" ]; then
-	echo "Impossible d'&#233;crire sur le disque USB"
+	echo "Impossible d'ecrire sur le disque USB"
 	EXIT1=1
 fi
 fi
 
-if [ "$BPCMEDIA" = "3" ]; then
+if [ "$bpcmedia" = "3" ]; then
 
-##### Test si le disque NAS est mont&#233; #####
-
-NAS_mntsuffix=`mysql se3db -u $dbuser -p$dbpass -B -N -e "select value from params where name='NAS_mntsuffix'"`
-NAS_proto=`mysql se3db -u $dbuser -p$dbpass -B -N -e "select value from params where name='NAS_proto'"`
+##### Test si le disque NAS est monte #####
+# NAS_mntsuffix=`mysql se3db -u $dbuser -p$dbpass -B -N -e "select value from params where name='NAS_mntsuffix'"`
+# NAS_proto=`mysql se3db -u $dbuser -p$dbpass -B -N -e "select value from params where name='NAS_proto'"`
 mount |grep "\/var\/lib\/backuppc" | grep "$NAS_proto" >/dev/null || MBPC=0
 if [ "$MBPC" = "0" ]; then
-	echo "Disque NAS non mont&#233;"
+	echo "Disque NAS non monte"
 	EXIT1=1
 fi
 touch /var/lib/backuppc/$NAS_mntsuffix/test 2&> /dev/null || TBPC=0
 if [ "$TBPC" = "0" ]; then
-	echo "Impossible d'&#233;crire sur le disque NAS"
+	echo "Impossible d'ecrire sur le disque NAS"
 	EXIT1=1
 fi
 fi
 
+
+if [ "$1" = "cron" ]; then
+	if [ -e  /var/run/backuppc/BackupPC.pid ]; then
+		if [ "$bpcmedia" = "0" ]; then
+			# on teste le chemin
+			CHEMIN_SAV="/var/lib/backuppc"
+			[ -h "$CHEMIN_SAV" ] && CHEMIN_SAV=$(readlink -e /var/lib/backuppc)
+			df |grep "$CHEMIN_SAV" >/dev/null || MBPC=0
+			if [ "$MBPC" = "0" ]; then
+				echo "Aucune paritition montee sur $CHEMIN_SAV"
+				EXIT1=1
+				
+			fi
+	fi
+	else
+		if [ $backuppc = "1" ]; then
+			echo "Attention, le module sauvegarde est actif mais le service backuppc est off"
+			exit 1
+		fi	
+	exit 0
+	fi
+
+fi
+	
+
 if [ "$EXIT1" = 1 ]; then
+	echo "Arret du service backuppc"
+	/etc/init.d/backuppc stop 
 	exit 1
 fi
+
+
